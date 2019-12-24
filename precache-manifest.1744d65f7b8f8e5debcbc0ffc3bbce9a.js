c77c7df5a63bfcbe92c4f5f747020778a9278d69
hamdizubir <hamdizubir@gmail.com>
2019-12-24 17:45:45 +0700

    Updates

---
+++ precache-manifest.1744d65f7b8f8e5debcbc0ffc3bbce9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4837076b7826f5134baa16084b44ac6",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "fb4c18238fc4c99e37e4",
+    "url": "/monster-rolodex/static/css/main.393370f1.chunk.css"
+  },
+  {
+    "revision": "12ed7e1fae371b4cb936",
+    "url": "/monster-rolodex/static/js/2.e494a0c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.e494a0c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fb4c18238fc4c99e37e4",
+    "url": "/monster-rolodex/static/js/main.73190486.chunk.js"
+  },
+  {
+    "revision": "939c7cac93c69c649cbc",
+    "url": "/monster-rolodex/static/js/runtime-main.11039a90.js"
+  }
+]);
